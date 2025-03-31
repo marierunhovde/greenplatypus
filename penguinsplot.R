403a62751242e0df6533ca215c9785c2454b5f62
marierunhovde <marierunhovde@gmail.com>
2025-03-31 13:32:53 +0200

    Updated penguinsplot.R

--- penguinsplot.R
+++ penguinsplot.R
@@ -2,7 +2,7 @@ library(tidyverse)
 library(palmerpenguins)
 
 
-ggplot(data = penguins, aes(x = bill_length_mm, y = species, color = species)) +
+ggplot(data = penguins, aes(x = bill_length_mm, y = bill_depth_mm, color = species)) +
   labs(x = "Bill length (mm)", y = "Species") +
   scale_color_viridis_d() + 
   geom_jitter() + 
