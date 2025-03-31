d15188729fca0457480b23f37a9a57482904bead
marierunhovde <marierunhovde@gmail.com>
2025-03-31 13:34:19 +0200

    changed to geom_point

--- penguinsplot.R
+++ penguinsplot.R
@@ -4,6 +4,6 @@ library(palmerpenguins)
 
 ggplot(data = penguins, aes(x = bill_length_mm, y = bill_depth_mm, color = species)) +
   labs(x = "Bill length (mm)", y = "Species") +
-  geom_jitter() + 
+  geom_point() + 
   theme_bw()
 
