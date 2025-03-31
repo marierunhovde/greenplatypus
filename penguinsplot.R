553720cf8e3e5093bf048798783bd89a0b10acac
marierunhovde <marierunhovde@gmail.com>
2025-03-31 10:57:49 +0200

    changed the color scheme

--- penguinsplot.R
+++ penguinsplot.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 library(palmerpenguins)
 
-ggplot(data = penguins, aes(x = bill_length_mm, y = species, color = species), labs = ) +
+ggplot(data = penguins, aes(x = bill_length_mm, y = species, color = species)) +
+  labs(x = "Bill length (mm)", y = "Species") +
   geom_point() + 
   theme_minimal()
