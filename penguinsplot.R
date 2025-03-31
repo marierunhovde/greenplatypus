8f6c190eac1380ce1b24b7ed89e4a0b6b256192c
marierunhovde <marierunhovde@gmail.com>
2025-03-31 10:51:55 +0200

    Formatted penguins plot

--- penguinsplot.R
+++ penguinsplot.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(palmerpenguins)
 
-ggplot(data = penguins, aes(x = bill_length_mm, y = species, color = "red")) +
-       geom_point()
+ggplot(data = penguins, aes(x = bill_length_mm, y = species, color = species), labs = ) +
+  geom_point() + 
+  theme_minimal()
