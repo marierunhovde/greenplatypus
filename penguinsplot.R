0e3863b1082ce9c265e875e3946e0033e2401861
marierunhovde <marierunhovde@gmail.com>
2025-03-31 13:30:02 +0200

    removed color theme

--- penguinsplot.R
+++ penguinsplot.R
@@ -4,7 +4,6 @@ library(palmerpenguins)
 
 ggplot(data = penguins, aes(x = bill_length_mm, y = species, color = species)) +
   labs(x = "Bill length (mm)", y = "Species") +
-  scale_color_viridis_d() + 
   geom_jitter() + 
   theme_bw()
 
